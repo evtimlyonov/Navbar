7a18da1a1c589a12e040283f324df6ac5e07b3b5
Evtim Lyonov <evtim.lyonov@gmail.com>
2022-02-15 13:24:24 +0200

    Desctop version done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Header } from './components/Header';
+
 function App() {
-  return <div className='App'>hello</div>;
+  return (
+    <main>
+      <Header />
+    </main>
+  );
 }
 
 export default App;
